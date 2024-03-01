178871c2d362038302503116085c281ab8b3cc8a
Viktor Zdravkovski <viktorzdravkovski5@gmail.com>
2024-03-01 11:11:19 +0100

    prva domashna c#

---
+++ Homework01/Homework 01/Homework 01/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.WriteLine("Homework");
+
+//#region Task1
+//Console.WriteLine("RealCalculator");
+
+//int res;
+//Console.WriteLine("Vnesi broj 1: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Vnesi broj 2: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Vnesi simbol: +, -, *, /");
+//string symbol = Console.ReadLine();
+
+//switch (symbol)
+//{
+//    case "+":
+//        res = num1 + num2;
+//        Console.WriteLine("Brojot e: " + res);
+//        break;
+//    case "-":
+//        res = num1 - num2;
+//        Console.WriteLine("Brojot e: " + res);
+//        break;
+//    case "*":
+//        res = num1 * num2;
+//        Console.WriteLine("Brojot e: " + res);
+//        break;
+//    case "/":
+//        res = num1 / num2;
+//        Console.WriteLine("Brojot e: " + res);
+//        break;
+//}
+
+//Console.ReadLine();
+//#endregion
+
+//#region Task2
+//Console.WriteLine("AverageNumber");
+
+//double number1, number2, number3, number4;
+
+//Console.WriteLine("Vnesi broj 1: ");
+//number1 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Vnesi broj 2: ");
+//number2 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Vnesi broj 3: ");
+//number3 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("Vnesi broj 4: ");
+//number4 = Convert.ToDouble(Console.ReadLine());
+
+//double result = (number1 + number2 + number3 + number4) / 4;
+//Console.WriteLine("Rezultatot e: " + result);
+//#endregion
+
+//#region Task3
+//Console.WriteLine("SwapNumbers");
+
+//Console.WriteLine("Vnesi broj 1: ");
+//int firstNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Vnesi broj 2: ");
+//int secondNumber = int.Parse(Console.ReadLine());
+
+//int temp = firstNumber;
+//firstNumber = secondNumber;
+//secondNumber = temp;
+
+//Console.WriteLine("Brojot 1 e: " + firstNumber);
+//Console.WriteLine("Brojot 2 e: " + secondNumber);
+
+//Console.ReadLine();
+//#endregion
