9bcfdc548629b28d8d1954346213e5f44fb66cbd
Viktor Zdravkovski <viktorzdravkovski5@gmail.com>
2024-03-09 13:08:33 +0100

    homework3

---
+++ Homework3/Homework03/Exercise02/Program.cs
@@ -0,0 +1,62 @@
+﻿//Write a method that formats a given date and time as a string in a specific format.
+// Get the needed inputs from screen.
+
+//Allowed formats: "MM/dd/yyyy", "MM/dd/yyyy hh:mm:ss", "dddd, dd MMMM yyyy HH:mm:ss", "MM.dd.yyyy"
+//Bonus: Create separate method that validates whether the entered format is allowed
+
+//static void everyFormat()
+//{
+//    Console.WriteLine("Please enter a date and time in the following format MM/dd/yyyy hh:mm:ss:");
+//    string Yearinput = Console.ReadLine();
+
+//    DateTime dateTime;
+//    if (DateTime.TryParse(Yearinput, out dateTime))
+//    {
+//        Console.WriteLine("Formatted date is: ");
+//        Console.WriteLine(dateTime.ToString("MM/dd/yyyy"));
+//        Console.WriteLine(dateTime.ToString("MM/dd/yyyy hh:mm:ss"));
+//        Console.WriteLine(dateTime.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+//        Console.WriteLine(dateTime.ToString("MM.dd.yyyy"));
+//    }
+//    else
+//    {
+//        Console.WriteLine("Invalid date and time format. Please try again.");
+//    }
+//}
+
+//everyFormat();
+
+// 
+
+
+
+static void stockFormat()
+{
+    DateTime customdate = new DateTime(2000, 8, 20, 5, 20, 35);
+    Console.Write("what format would you like to be printed \n \"MM/dd/yyyy\" \n MM/dd/yyyy hh:mm:ss \n dddd, dd MMMM yyyy HH:mm:ss \n MM.dd.yyyy \n \n");
+    string formatedString = Console.ReadLine();
+
+    if (formatedString == "MM/dd/yyyy".ToLower())
+    {
+        Console.WriteLine(customdate.ToString("MM / dd / yyyy"));
+
+    }
+    else if (formatedString == "MM/dd/yyyy hh:mm:ss".ToLower())
+    {
+        Console.WriteLine(customdate.ToString("MM/dd/yyyy hh:mm:ss".ToLower()));
+    }
+    else if (formatedString == "dddd, dd MMMM yyyy HH:mm:ss".ToLower())
+    {
+        Console.WriteLine(customdate.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+    }
+    else if (formatedString == "MM.dd.yyyy".ToLower())
+    {
+        Console.WriteLine(customdate.ToString("MM.dd.yyyy"));
+    }
+    else
+    {
+        Console.WriteLine("Entered format is allowed not allowed ");
+    }
+}
+
+stockFormat();
---
+++ Homework3/Homework03/Homework03/Program.cs
@@ -0,0 +1,96 @@
+﻿// Make a method called AgeCalculator
+// The method will have one input parameter, your birthday date
+// The method should return your age
+// Show the age of a user after he inputs a date
+
+#region BasicYearCalc
+//static void main()
+//{
+//    Console.WriteLine("Vnesi godina vo koja si roden: ");
+//    int birthyear = int.Parse(Console.ReadLine());
+//    DateTime dateTime = DateTime.Now;
+//    int novagod = dateTime.Year - birthyear;
+//    Console.WriteLine("Imate " + novagod + " Godini");
+//}
+
+//main();
+#endregion
+
+#region ageCalc Without method
+//Console.WriteLine("Todays date is " + DateTime.Now.ToLongDateString());
+//Console.WriteLine("Enter your birthdate (MM/dd/yyyy):");
+//DateTime inputBirthdate = DateTime.Parse(Console.ReadLine());
+
+//DateTime currentDate = DateTime.Now;
+//int age = currentDate.Year - inputBirthdate.Year;
+
+//if (currentDate.Month < inputBirthdate.Month || (currentDate.Month == inputBirthdate.Month && currentDate.Day < inputBirthdate.Day))
+//{
+//    age--;
+//    Console.WriteLine(age);
+//}
+//else
+//{
+//    Console.WriteLine("Your age is: " + age);
+//}
+#endregion
+
+#region AgeCalc with VOID method
+
+//static void Agecalc()
+//{
+//    Console.WriteLine("When were you born?: (mm/dd/yyyy)");
+//    DateTime inputBirthdate = DateTime.Parse(Console.ReadLine());
+
+//    DateTime currentDate = DateTime.Now;
+//    int ageCalculator = currentDate.Year - inputBirthdate.Year;
+
+//    if (currentDate.Month < inputBirthdate.Month || (currentDate.Month == inputBirthdate.Month && currentDate.Day < inputBirthdate.Day))
+//    {
+//        ageCalculator--;
+//        Console.WriteLine(ageCalculator);
+//    }
+//    else
+//    {
+//        Console.WriteLine(ageCalculator);
+//    }
+//}
+
+//Agecalc();
+
+#endregion
+
+#region AgeCalc with input Parameter
+Console.WriteLine("Todays date is " + DateTime.Now.ToLongDateString());
+static int AgeCalculator(DateTime birthdate)
+{
+    DateTime currentDate = DateTime.Now;
+    int age = currentDate.Year - birthdate.Year;
+
+    if (currentDate.Month < birthdate.Month || (currentDate.Month == birthdate.Month && currentDate.Day < birthdate.Day))
+    {
+        age--;
+        return age;
+    }
+
+    return age;
+}
+static void Main()
+{
+    Console.WriteLine("Please enter your birthdate in the format MM/dd/yyyy:");
+    string birthdateInput = Console.ReadLine();
+
+    DateTime inputBirthdate;
+    if (DateTime.TryParse(birthdateInput, out inputBirthdate))
+    {
+        int age = AgeCalculator(inputBirthdate);
+        Console.WriteLine("Your age is: " + age);
+    }
+    else
+    {
+        Console.WriteLine("Invalid birthdate format. Please try again.");
+    }
+}
+Main();
+
+#endregion
