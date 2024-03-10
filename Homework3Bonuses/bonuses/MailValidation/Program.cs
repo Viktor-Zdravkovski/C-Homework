23adbf311e983b41b93fc3e992cefedca2cebf30
Viktor Zdravkovski <viktorzdravkovski5@gmail.com>
2024-03-10 01:39:38 +0100

    Bonus Homework 4

---
+++ Homework3Bonuses/bonuses/MailValidation/Program.cs
@@ -0,0 +1,56 @@
+﻿// BONUS 2
+
+//Create a method that accepts a string, checks if it's a valid email address and returns either true or false, depending on the evaluation.
+
+//The string must contain an @ character.
+//The string must contain a . character.
+//The @ must have at least one character in front of it.
+//ex. "e@mail.com" is valid, while "@email.com" is invalid.
+//The . and the @ must be in the appropriate places.
+//ex. "hello.email@com" is invalid while "john.smith@email.com" is valid.
+//If the string passes these tests, it's considered a valid email address.
+
+//ValidateEmail("@gmail.com") ➞ false
+
+//ValidateEmail("hello.gmail@com") ➞ false
+
+//ValidateEmail("gmail") ➞ false
+
+//ValidateEmail("hello@gmail") ➞ false
+
+//ValidateEmail("hello@gmail.com") ➞ true
+
+Console.WriteLine("===== BONUS 2 =====");
+
+Console.Write("Enter your email here: ");
+string inputEmailValue = Console.ReadLine();
+static bool EmailValidation(string email)
+{
+    if (!email.Contains('@') || !email.Contains('.'))
+    {
+        return false;
+    }
+
+    int atIndex = email.IndexOf('@');
+    if (atIndex == 0)
+    {
+        return false;
+    }
+
+    int dotIndex = email.LastIndexOf('.');
+    if (dotIndex < atIndex + 2 || dotIndex == email.Length - 1)
+    {
+        return false;
+    }
+
+    if (email.LastIndexOf('.') == email.Length - 1)
+    {
+        return false;
+    }
+    return true;
+
+}
+string emailValidation = $"The input email after evaluation has returned: {EmailValidation(inputEmailValue)}";
+Console.WriteLine(emailValidation);
+
+
---
+++ Homework3Bonuses/bonuses/bonuses/Program.cs
@@ -0,0 +1,48 @@
+﻿// BONUS 1
+
+// Given a string, create a method to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+//ReverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+
+//ReverseCase("MANY THANKS") ➞ "many thanks"
+
+//ReverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+
+Console.WriteLine("==== BONUS 1 ====");
+
+static string UpperLower(string sdfgsdg)
+{
+
+    Console.Write("Write a word that you want to be reversed: ");
+    //string inputWordValue = Console.ReadLine();
+    string inputWordValue = sdfgsdg;
+    char[] inputCharsValue = inputWordValue.ToCharArray();
+    char[] newCharArray = new char[inputCharsValue.Length];
+
+    for (int i = 0; i < inputCharsValue.Length; i++)
+    {
+        if (Char.IsLower(inputCharsValue[i]))
+        {
+            newCharArray[i] = char.ToUpper(inputCharsValue[i]);
+        }
+        else if (Char.IsUpper(inputCharsValue[i]))
+        {
+            newCharArray[i] = char.ToLower(inputCharsValue[i]);
+        }
+        else
+        {
+            newCharArray[i] = inputCharsValue[i];
+        }
+    }
+    string reversedString = new string(newCharArray);
+    return reversedString;
+
+}
+string reversedWord = UpperLower("Happy BirthDay");
+string reverseWord1 = UpperLower("MANY THANKS");
+string reverseWord2 = UpperLower("sPoNtAnEoUs");
+
+Console.WriteLine(reversedWord);
+Console.WriteLine(reverseWord1);
+Console.WriteLine(reverseWord2);
+
+
