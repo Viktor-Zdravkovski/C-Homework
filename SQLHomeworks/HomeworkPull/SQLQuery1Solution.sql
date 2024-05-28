922cc151a54bd4cbc11b2e2dfda17fe790d49258
Viktor Zdravkovski <viktorzdravkovski5@gmail.com>
2024-05-28 22:23:35 +0200

    28.05.2024

--- SQLHomeworks/HomeworkPull/SQLQuery1Solution.sql
+++ SQLHomeworks/HomeworkPull/SQLQuery1Solution.sql
@@ -149,3 +149,55 @@ FROM VW_StudentGrades
 ORDER BY GradeCount DESC
 
 --//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+--//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- Homework 28.05.2024
+
+--Declare scalar variable for storing FirstName values
+DECLARE @FirstName NVARCHAR(50)
+
+--• Assign value ‘Antonio’ to the FirstName variable
+SET @FirstName = 'Antonio'
+SELECT @FirstName
+
+--• Find all Students having FirstName same as the variable
+SELECT *
+FROM [dbo].[Student]
+WHERE FirstName = @FirstName
+
+--• Declare table variable that will contain StudentId, StudentName and DateOfBirth
+DECLARE @StudentList TABLE (StudentId INT, StudentName NVARCHAR(100), DateOfBirth DATE)
+
+--• Fill the table variable with all Female students
+INSERT INTO @StudentList (StudentId, StudentName, DateOfBirth)
+SELECT Id, FirstName, DateOfBirth
+FROM [dbo].[Student]
+WHERE Gender = 'F'
+
+SELECT *
+FROM @StudentList
+
+
+--• Declare temp table that will contain LastName and EnrolledDate columns
+CREATE TABLE #LastNameAndEnrolledDate (LastName NVARCHAR(100), EnrolledDate DATE)
+
+--• Fill the temp table with all Male students having First Name starting with ‘A’
+INSERT INTO #LastNameAndEnrolledDate (LastName, EnrolledDate)
+SELECT LastName, EnrolledDate
+FROM [dbo].[Student]
+WHERE Gender = 'M' AND FirstName LIKE 'A%'
+
+SELECT *
+FROM #LastNameAndEnrolledDate
+
+--• Retrieve the students from the table which last name is with 7 characters
+SELECT *
+FROM [dbo].[Student]
+WHERE LEN(LastName) = 7
+
+--• Find all teachers whose FirstName length is less than 5 and the first 3 characters of their FirstName and LastName are the same.
+SELECT *
+FROM [dbo].[Teacher]
+WHERE LEN(FirstName) < 5 AND LEFT(FirstName, 3) = LEFT(LastName, 3)
+
